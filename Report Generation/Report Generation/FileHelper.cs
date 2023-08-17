db0ffe2421ceacae144018cbe3f0418752a57a99
Leaf8910 <100060220+Leaf8910@users.noreply.github.com>
2023-08-17 23:31:32 +0800

    Add project files.

---
+++ Report Generation/Report Generation/FileHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Report_Generation; // Adjust the namespace as needed
+
+namespace Report_Generation
+{
+    public static class FileHelper
+    {
+        public static string ReadAssetFile(string filename)
+        {
+            string content;
+            try
+            {
+                // Get the AssetManager from the Android context
+                using (StreamReader sr = new StreamReader(Android.App.Application.Context.Assets.Open(filename)))
+                {
+                    content = sr.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                content = $"Error reading file: {ex.Message}";
+            }
+            return content;
+        }
+    }
+}
